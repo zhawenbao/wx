a69246398e151baa1ab2a8177e7287ed88b7d434
GreatMe <863973855@qq.com>
2019-03-09 22:14:32 +0800

    菜单

--- application/wx/controller/Wx.php
+++ application/wx/controller/Wx.php
@@ -135,49 +135,41 @@ class WX extends Controller
         $api = 'https://api.weixin.qq.com/cgi-bin/menu/create?access_token='.$access_token;
         $menuData = '
             {
-		        "menu":{
-		            "button":
-                        [
+                "button":[
+                    {    
+                        "type": "view", 
+                        "name": "百度搜索", 
+                        "url": "http://www.baidu.com", 
+                    },
+                    {    
+                        "type": "view", 
+                        "name": "随心听", 
+                        "url": "http://www.5nd.com/fm/app.html", 
+                    },
+                    {
+                        "name":"商城",
+                        "sub_button":[
                             {    
-                                "type": "view", 
-                                "name": "百度搜索", 
-                                "url": "http://www.baidu.com", 
-                                "sub_button": [ ]
-                            },
-                            {    
-                                "type": "view", 
-                                "name": "随心听", 
-                                "url": "http://www.5nd.com/fm/app.html", 
-                                "sub_button": [ ]
+                                "type":"view",
+                                "name":"京东商城",
+                                "url":"http://www.jd.com"
                             },
                             {
-                                "name":"商城",
-                                "sub_button":
-                                [
-                                    {    
-                                        "type":"view",
-                                        "name":"京东商城",
-                                        "url":"http://www.jd.com"
-                                    },
-                                    {
-                                        "type":"view",
-                                        "name":"苏宁易购",
-                                        "url": "https://www.suning.com"
-                                    },
-                                    {
-                                        "type":"view",
-                                        "name":"天猫商城",
-                                        "url": "https://s.click.taobao.com"
-                                    },
-                                    {
-                                        "type":"view",
-                                        "name":"拼多多",
-                                        "url": "https://www.pinduoduo.com"
-                                    },
-                                ]
+                                "type":"view",
+                                "name":"苏宁易购",
+                                "url": "https://www.suning.com"
                             },
-                        ]
-		        }    
+                            {
+                                "type":"view",
+                                "name":"天猫商城",
+                                "url": "https://s.click.taobao.com"
+                            },
+                            {
+                                "type":"view",
+                                "name":"拼多多",
+                                "url": "https://www.pinduoduo.com"
+                            },]
+                    },]
 		    }';
         $res = $this->post($api, $menuData);
     }
