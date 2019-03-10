26a8efaac823f3d1708340dad36dd877cc5a1904
GreatMe <863973855@qq.com>
2019-03-10 14:53:48 +0800

    微信菜单设置点击事件

--- application/wx/controller/Wx.php
+++ application/wx/controller/Wx.php
@@ -132,11 +132,7 @@ class WX extends Controller
     //詳情介紹
     private function content()
     {
-        return "
-            1.编辑部：010-57368789,010-57368790 /r/n
-            2.微信推广 /r/n
-            3.广告、理事会
-        ";
+        return "1.编辑部：010-57368789,010-57368790/r/n2.微信推广/r/n3.广告、理事会";
     }
 
     // 语音消息回复
