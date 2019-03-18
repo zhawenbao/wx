b7df3bea34d581e9741080dc609ece27844f2d63
GreatMe <863973855@qq.com>
2019-03-18 13:39:27 +0800

    图文

--- application/wx/controller/Wx.php
+++ application/wx/controller/Wx.php
@@ -211,6 +211,7 @@ class WX extends Controller
             ];
             $resultStr = $this->news($content);
         }
+        file_put_contents('news.txt', json_encode($resultStr));
         return $resultStr;
     }
 
@@ -391,7 +392,6 @@ class WX extends Controller
 
         $resultStr = sprintf($newsTpl, $this->postObj->FromUserName, $this->postObj->ToUserName, time(),count($content));
 //        file_put_contents('number.txt', count($content));
-        file_put_contents('news.txt', json_encode($newsTpl));
         return $resultStr;
     }
     //查詢地址
